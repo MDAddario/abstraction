fc6de8a027d95518d0ad2dc181bc464c99ec7822
MDAddario <michael.lindner.daddario@gmail.com>
2020-08-01 01:00:07 -0400

    Updates the driver print statements

--- driver.py
+++ driver.py
@@ -5,7 +5,8 @@ from control import control_dict
 
 if __name__ == '__main__':
     
-    with suppress(KeyError):
-        Pencil = control_dict['pencil']
-        Pencil.process_in()
-        Pencil.process_out()
+    for key, controller in control_dict.items():
+        with suppress(KeyError):
+            print(f"The key is {key}:")
+            controller.process_in()
+            controller.process_out()
