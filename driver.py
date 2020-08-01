90173627aa4cf43ab10e86a0e51bedb19af3749a
MDAddario <michael.lindner.daddario@gmail.com>
2020-08-01 01:15:23 -0400

    Dont need key error suppression anymore

--- driver.py
+++ driver.py
@@ -1,13 +1,9 @@
-from contextlib import suppress
-
 from control import control_dict
 
 
 if __name__ == '__main__':
     
     for key, controller in control_dict.items():
-        with suppress(KeyError):
-            print(f"The key is {key}:")
-            controller.process_in()
-            controller.process_out()
-            print('')
+        print(f"\nThe key is {key}:")
+        controller.process_in()
+        controller.process_out()
